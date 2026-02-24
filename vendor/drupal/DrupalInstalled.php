397cbb32dc2dce8fb1792349bf639e06c63047d3
Collin Stewart <collin.stewart@itd.sbcounty.gov>
2026-02-24 00:34:38 -0800

    commit

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '0527b115f1cbbcb1';
+  public const string VERSIONS_HASH = '6f5bdaa49189456c';
 
 }
